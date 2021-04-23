c5a9d9cb53624a1e040bd5192134048861c79ab8
sanjana k <sanjanarthi@gmail.com>
2021-04-24 00:14:44 +0530

    Day33_Added While_loop_4

---
+++ While_loop/While_loop_4.js
@@ -0,0 +1,36 @@
+/* Find the Magic Number
+a. Ask the user to think of a number n between 1 to 100
+b. Then check with the user if the number is less then n/2 or greater
+c. Repeat till the Magic Number is reached..  */
+
+console.log("choose a numer between 1 and 100");
+const readline = require('readline-sync');
+var lowest = 1;
+var highest = 100;
+var mid = 50;
+var flag = 0;
+while (lowest < highest) {
+    console.log("enter 1 if number is b/w "+ lowest +" and " + mid)
+    console.log("enter 2 if number is b/w "+ mid +" and " + highest)
+    console.log("enter 3 if number is equal to "+ mid)
+   let value = readline.questionInt();
+    switch (value) {
+        case 1:
+          highest = mid - 1;
+        break;
+        case 2:
+          lowest = mid + 1;
+        break;    
+        case 3:
+          flag++;
+        break;
+        default:
+          text = "oops! wrong choice";
+        break;         
+    }
+    mid = parseInt((lowest + highest)/2);
+    if(flag == 1){
+        break;
+    }
+}
+console.log("your value is = " + mid)
