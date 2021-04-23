da213eabb04646a1dcfe9855ed2077dfc309d0bf
sanjana k <sanjanarthi@gmail.com>
2021-04-23 21:46:21 +0530

    Day33_Added Use REPL_3

---
+++ SequenceSteps/Use_Repl_3.js
@@ -0,0 +1,4 @@
+let dice1 = Math.floor(Math.random() * 10) % 6 + 1;
+let dice2 = Math.floor(Math.random() * 10) % 6 + 1;
+let result = dice1 + dice2;
+console.log("Addition of two random dice numbers "+ dice1 +"+"+ dice2+ "=" + result);
