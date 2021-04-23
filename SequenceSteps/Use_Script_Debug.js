990be23e3d552b0206c48a8332c1ef5d2a3ef286
sanjana k <sanjanarthi@gmail.com>
2021-04-23 21:48:42 +0530

    Day33_Added Use Script and Debug_4

---
+++ SequenceSteps/Use_Script_Debug.js
@@ -0,0 +1,8 @@
+let sum = 0;
+for (i = 0; i<5; i++) {
+let number = Math.floor(Math.random() * 90) + 10;
+ sum += number;
+}
+let average = sum/5;
+console.log("The sum of  5 Random 2 Digit values is : "+ sum);
+console.log("The average of  5 Random 2 Digit values is : "+ average);
